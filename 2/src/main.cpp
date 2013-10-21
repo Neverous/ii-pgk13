006fb1e67babe0811bcfda6684a86c86839b7416
Maciej Szeptuch <neverous@neverous.info>
2013-10-21 21:04:40 +0200

    Fixing compilation under windows.

--- 2/src/main.cpp
+++ 2/src/main.cpp
@@ -22,8 +22,8 @@ int main(void)
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
